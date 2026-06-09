0d44061657878d52de960e79922bc015c32b8ce6
noda-chpartners <noda@ch-partners.co.jp>
2026-06-09 14:40:56 +0900

    aaaa

--- src/pages/home/components/AboutSection.tsx
+++ src/pages/home/components/AboutSection.tsx
@@ -1,5 +1,5 @@
 import { useScrollAnimation } from '@/hooks/useScrollAnimation';
-import about from '../../../assets/use/about.jpg';
+import about from '../../assets/use/about.jpg';
 
 export default function AboutSection() {
   const { ref, isVisible } = useScrollAnimation(0.15);
